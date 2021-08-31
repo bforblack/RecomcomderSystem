4355af90e6cd268c9aceb5cd94d98d2983f8a9fd
Shivankur Tripathi <shivankur.tripathi@crmnext.com>
2021-08-31 08:13:01 +0530

    wip

--- ContentBasesdSuggestion.py
+++ ContentBasesdSuggestion.py
@@ -3,6 +3,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import pandas as pd
 from DataExtracter import FetchMovieData
+from ast import literal_eval
+import numpy as np
 
 
 class ContentBasesdSuggestion:
@@ -16,7 +18,34 @@ class ContentBasesdSuggestion:
         cosin_similarity=linear_kernel(tdifmetrices,tdifmetrices)
         dataIndex=pd.Series(data.index,index=data.title)
         index=dataIndex[movieName]
-        prediction=cosin_similarity[index]
+        prediction=sorted(list(enumerate(cosin_similarity[index])),reverse=True)
+        final_list=prediction[1:10]
+        movieList=[]
+        for li in final_list:
+            movieList.append(data.title[li[0]])
+        print(movieList)
+        columnList=['cast','crew','genres','keywords','production_companies']
+        for field in columnList :
+            data[field]=data[field].apply(literal_eval)
+        data['director']=data['crew'].apply(self.fetch_director_data)
+
+
+    def fetch_director_data(self,actor_data):
+        for j in actor_data:
+                if j['job'] == 'Director':
+                    return j['name']
+
+        return np.nan
+
+
+
+
+
+
+
+
+
+
 
 
 
--- DataExtracter.py
+++ DataExtracter.py
@@ -4,8 +4,8 @@ import logging
 class FetchMovieData:
 
     def generateData(self):
-        data=pd.read_csv('C:/Users/ShivankarT/Pictures/MovieDataset/tmdb_5000_credits.csv')
-        data2=pd.read_csv("C:/Users/ShivankarT/Pictures/MovieDataset/tmdb_5000_movies.csv")
+        data=pd.read_csv('C:/Users/Shivankur Tripathi/Downloads/archive/tmdb_5000_credits.csv')
+        data2=pd.read_csv("C:/Users/Shivankur Tripathi/Downloads/archive/tmdb_5000_movies.csv")
         logging.info("Data Read")
         finaldata=pd.merge(data,data2)
         logging.info("Shape",finaldata.shape)
